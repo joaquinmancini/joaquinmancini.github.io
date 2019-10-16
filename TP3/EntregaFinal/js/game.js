745be070476cff71ffb4303620c470def70c32ce
Joaquin Mancini <andrx_it@hotmail.com>
2019-10-16 11:33:16 -0300

    fix dot on life.style

--- TP3/EntregaFinal/js/game.js
+++ TP3/EntregaFinal/js/game.js
@@ -88,7 +88,7 @@ export function startGame() {
     let inicio = document.querySelector('.inicio');
     inicio.addEventListener('click', function () {
         distCounter = 0;
-        lifestyle.width = this.lAux + "px";
+        life.style.width = this.lAux + "px";
         adventurer.animation = "anRunA 1s steps(8) infinite";
         myID = requestAnimationFrame(loop);
     })
